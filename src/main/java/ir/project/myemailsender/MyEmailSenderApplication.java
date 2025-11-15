30970e5c668c1f1900093a117e3202166474b87f
Paniii6743 <paniiisafa@gmail.com>
2025-11-14 22:51:56 -0800

    first commit

---
+++ src/main/java/ir/project/myemailsender/MyEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package ir.project.myemailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEmailSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyEmailSenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/project/myemailsender/MyEmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.project.myemailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEmailSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
